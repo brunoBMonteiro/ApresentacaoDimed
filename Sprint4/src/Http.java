1f88e62754badf0191f8492a8a2a009db6ce5694
tomWindwos <fichtner.dk@hotmail.com>
2022-01-21 09:15:59 -0300

    add project Sprint 4

---
+++ Sprint4/src/Http.java
@@ -0,0 +1,20 @@
+public class Http {
+    // Protocolo Http
+
+   // H - Hyper T - Text
+   // T - Transfer
+   // P - Protocol
+
+    // Protocolo de Transferencia de Hipertexto
+
+    // HiperTexto - Nada mais é que um Textao ( Ref. livros antigos)
+                // clica e ja vai direto pra Ref
+
+
+// Caracteristicas
+    // Camada de Aplicação - a mais alto nivel -
+    // é um protocolo Stateless 
+
+
+
+}
