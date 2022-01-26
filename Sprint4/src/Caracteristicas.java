a67699bff0dad5f0b115b59478fcfb2e47c18dbe
tomWindwos <fichtner.dk@hotmail.com>
2022-01-26 10:04:49 -0300

    #25 Exemple and http

---
+++ Sprint4/src/Caracteristicas.java
@@ -0,0 +1,83 @@
+public class Caracteristicas {
+
+/*
+    HyperText Transfer Protocol
+
+// H - Hyper T - Text
+// T - Transfer
+// P - Protocol
+
+    é um protocolo genérico
+    em nível de aplicação
+    stateless
+    é usado atualmente qualquer tipo de chamada (Cliente - Servidor )
+
+    HTTP/0.9 — The One-line Protocol
+    Methods supported: GET only
+    Connection nature: terminated immediately after the response
+    No Headers,No status/error codes, No URLs, No versioning
+
+    HTTP/1.0 — Building extensibility
+    Methods supported: GET , HEAD , POST
+    HTTP version number, status code, content type
+    Connection nature: terminated immediately after the response
+
+    HTTP/1.1 — The standardized protocol
+    Methods supported: GET , HEAD , POST , PUT , DELETE , TRACE , OPTIONS
+    Connection nature: long-lived
+
+
+ */
+
+
+  // https://user-images.githubusercontent.com/33350348/150981148-376cb11c-790e-43e8-aa4d-4ffe58bb65bd.png
+
+/*
+
+    Características
+            Simplicidade: foi projetado para ser simples e legível às pessoas.
+
+    Extensibilidade:
+            variáveis de cabeçalho personalizadas
+            chave de acesso secreta a determinado recurso
+
+    Sessões:
+            HTTP não tem estado
+            cookies
+
+*/
+
+/*
+//https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Methods
+
+    Requisição : GET, POST, DELETE, PUT
+
+    GET : Solicita a leitura de informações de um recurso no servidor.  Ex: imagem
+
+    POST: Envio de informações/dados para o Bd
+
+    DELETE: Exclusão recurso, exemplo tirar item do carrinho
+
+    PUT : Substituir  Dados BD
+
+    PATCH: Altera apenas uma parte, Ex: senha
+
+    HEAD
+        O método HEAD solicita uma resposta de forma idêntica ao método GET, porém sem conter o corpo da resposta.
+
+    CONNECT
+        O método CONNECT estabelece um túnel para o servidor identificado pelo recurso de destino.
+    OPTIONS
+        O método OPTIONS é usado para descrever as opções de comunicação com o recurso de destino.
+
+
+*/
+
+
+
+    // https://httpbin.org/
+
+
+    //curl -X OPTIONS http://example.org -i
+
+}
---
+++ Sprint4/src/HttpClientStatus.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class HttpClientStatus {
+          // The example creates a GET request to the webcode.me website and retrives an http response.
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        // Request
+
+        HttpRequest request = HttpRequest.newBuilder()
+                //  https://www.panvel.com/
+                .uri(URI.create("http://webcode.me"))
+                .GET() // GET is default
+                .build();
+
+        
+
+           // Response
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpResponse<Void> response = client.send  (request,HttpResponse.BodyHandlers.discarding());
+        System.out.println(response.statusCode());
+
+    }
+}
